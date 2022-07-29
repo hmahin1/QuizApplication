644f6bbe1182a085c47c433bd351a649fd0d3863
Mohammad Safwan <developer@ivolve.io>
2022-07-29 17:57:33 +0500

    change config

--- src/constants/apikey.js
+++ src/constants/apikey.js
@@ -1,11 +1,22 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyAdpOqanbLlr9aU_lVkjaie1Y_w_xLRcVU",
+//   authDomain: "mastermind-2382f.firebaseapp.com",
+//   databaseURL: "https://mastermind-2382f-default-rtdb.firebaseio.com",
+//   projectId: "mastermind-2382f",
+//   storageBucket: "mastermind-2382f.appspot.com",
+//   messagingSenderId: "637808510634",
+//   appId: "1:637808510634:web:ad44bb98f514ba960572d3",
+//   measurementId: "G-606T90BPYR"
+// };
+
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyAdpOqanbLlr9aU_lVkjaie1Y_w_xLRcVU",
-  authDomain: "mastermind-2382f.firebaseapp.com",
-  databaseURL: "https://mastermind-2382f-default-rtdb.firebaseio.com",
-  projectId: "mastermind-2382f",
-  storageBucket: "mastermind-2382f.appspot.com",
-  messagingSenderId: "637808510634",
-  appId: "1:637808510634:web:ad44bb98f514ba960572d3",
-  measurementId: "G-606T90BPYR"
-};
+  apiKey: "AIzaSyDOolxZfnzQY9AesXp7NQJGESY_bGqGjaM",
+  authDomain: "mastermind2-bc7df.firebaseapp.com",
+  projectId: "mastermind2-bc7df",
+  storageBucket: "mastermind2-bc7df.appspot.com",
+  messagingSenderId: "261246986832",
+  appId: "1:261246986832:web:53dbbb33171e33e8677f55",
+  measurementId: "G-QB6F4WGVR1"
+}
